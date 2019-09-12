c06034c06eb25f8721dde2ea3d2ac520485935f0
Max Rothrock <maxrothrock@gmail.com>
2019-09-12 15:16:58 -0700

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Max Rothrock on 9/12/19.
+//  Copyright © 2019 Max Rothrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
